a03465d4cf8941b9730f6149e4eb1c1da433434e
memelopez <meme30lopez@gmail.com>
2021-10-06 11:01:08 -0600

    Remove methods that dynamically added the title, form, and lower button

--- src/ui.js
+++ src/ui.js
@@ -9,72 +9,8 @@ import clearCompleted from './clearCompleted';
 
 export default class UI {
   static addApp() {
-    // this.addTitle();
-    // this.addForm();
-    // this.addEmptyUL();
     const todos = Store.getTasks();
     this.addTasksUI(todos);
-    // this.addbottombtn();
-  }
-
-  static addTitle() {
-    const appDiv = document.querySelector('#appDiv');
-
-    const div4title = document.createElement('div');
-    div4title.className = 'd-flex justify-content-start align-items-center border-bottom border-2 px-2 appItem';
-
-    const title = document.createElement('p');
-    title.className = 'fs-5 m-0';
-    title.innerText = "Today's To Do";
-
-    const icon = document.createElement('i');
-    icon.className = 'fas fa-sync-alt ms-auto p-2';
-
-    div4title.appendChild(title);
-    div4title.appendChild(icon);
-
-    appDiv.appendChild(div4title);
-  }
-
-  static addForm() {
-    const appDiv = document.querySelector('#appDiv');
-
-    const div4form = document.createElement('div');
-    const form = document.createElement('form');
-    form.className = 'd-flex justify-content-start align-items-center border-bottom border-2 px-2 appItem';
-    form.action = 'submit';
-    form.id = 'addTaskForm';
-
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.id = 'taskDesc';
-    input.placeholder = 'Add to your list...';
-    input.className = 'form-control border-0 fst-italic p-0';
-
-    const icon = document.createElement('i');
-    icon.className = 'fas fa-sign-in-alt ms-auto p-2';
-
-    const aSubmit = document.createElement('a');
-    aSubmit.setAttribute('id', 'clickEnterIcon');
-
-    form.appendChild(input);
-    aSubmit.appendChild(icon);
-    form.appendChild(aSubmit);
-    div4form.appendChild(form);
-
-    appDiv.appendChild(div4form);
-  }
-
-  static addEmptyUL() {
-    const appDiv = document.querySelector('#appDiv');
-
-    const div4list = document.createElement('div');
-    const list = document.createElement('ul');
-    list.id = 'task-list';
-    list.className = 'p-0 m-0';
-    div4list.appendChild(list);
-
-    appDiv.appendChild(div4list);
   }
 
   static addEmptyToDoMessage() {
@@ -158,21 +94,6 @@ export default class UI {
     item.appendChild(div4Icons);
 
     list.appendChild(item); // appends item to list
-  }
-
-  static addbottombtn() {
-    const appDiv = document.querySelector('#appDiv');
-
-    const btmDiv = document.createElement('div');
-    btmDiv.className = 'd-flex justify-content-center align-items-center border btmDiv';
-
-    const pBtm = document.createElement('p');
-    pBtm.textContent = 'Clear all completed';
-    pBtm.className = 'm-0 btmText';
-    pBtm.id = 'pBtm';
-
-    btmDiv.appendChild(pBtm);
-    appDiv.appendChild(btmDiv);
   }
 
   static addTaskStore(description) {
